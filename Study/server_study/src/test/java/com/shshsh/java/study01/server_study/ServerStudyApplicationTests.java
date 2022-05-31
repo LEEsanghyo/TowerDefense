23c3eab2e7017f8803beea7c4f5a21240737531c
LEEsanghyo <tunyou2005@gmail.com>
2022-05-31 22:39:45 +0900

    git_refactoring

---
+++ Study/server_study/src/test/java/com/shshsh/java/study01/server_study/ServerStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shshsh.java.study01.server_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
